9b47a524932028f1fee29f184fb6c297235fdb16
Jenny Kitchen <jennykayekitchen@gmail.com>
2023-03-01 15:38:55 -0600

    phase 2

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ namespace Quest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your name?");
+
+            
             // Create a few challenges for our Adventurer's quest
             // The "Challenge" Constructor takes three arguments
             //   the text of the challenge
@@ -43,8 +46,9 @@ namespace Quest
             int maxAwesomeness = 100;
 
             // Make a new "Adventurer" object using the "Adventurer" class
-            Adventurer theAdventurer = new Adventurer("Jack");
-
+            // read line is a method in system, like a function in js
+            Adventurer theAdventurer = new Adventurer(Console.ReadLine());
+            
             // A list of challenges for the Adventurer to complete
             // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
             List<Challenge> challenges = new List<Challenge>()
